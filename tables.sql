d0658cdaeaee04a52aab17f440fc3e71ae86547a
Eropso <156892352+Eropso@users.noreply.github.com>
2025-04-07 11:15:49 +0200

    change mariadb user for hosting

--- tables.sql
+++ tables.sql
@@ -11,18 +11,18 @@ CREATE TABLE `users` (
 )
 
 CREATE TABLE `products` (
-    `id` int(11) NOT NULL AUTO_INCREMENT,
-    `title` varchar(200) NOT NULL,
-    `description` mediumtext NOT NULL,
-    `price` decimal(7,2) NOT NULL,
-    `rrp` decimal(7,2) NOT NULL DEFAULT 0.00,
-    `quantity` int(11) NOT NULL,
-    `img` mediumtext NOT NULL,
-    `date_added` datetime NOT NULL DEFAULT current_timestamp(),
-  PRIMARY KEY (`id`)
+ `id` int(11) NOT NULL AUTO_INCREMENT,
+ `title` varchar(200) NOT NULL,
+ `description` mediumtext NOT NULL,
+ `price` decimal(7,2) NOT NULL,
+ `rrp` decimal(7,2) NOT NULL DEFAULT 0.00,
+ `quantity` int(11) NOT NULL,
+ `img` mediumtext NOT NULL,
+ `date_added` datetime NOT NULL DEFAULT current_timestamp(),
+ PRIMARY KEY (`id`)
 )
 
 
 INSERT INTO `products` (`id`, `title`, `description`, `price`, `rrp`, `quantity`, `img`, `date_added`) VALUES
 (1, 'T-shirt', '<p>High quality Cotton. Thick material GSM 500. Made in Italy.</p>', '29.99', '10.00', 10, 'tshirt.jpg', '2025-01-01 00:00:00'),
-(2, 'Sweatpants', '<p>Cotton poly blend. Made in Italy. Minimum Wage Workers.</p>', '20.99', '8.99', 34, 'sweatpants.jpg', '2025-01-01 00:00:00'),
+(2, 'Sweatpants', '<p>Cotton poly blend. Made in Italy. Minimum Wage Workers.</p>', '20.99', '8.99', 34, 'sweatpants.jpg', '2025-01-01 00:00:00')
