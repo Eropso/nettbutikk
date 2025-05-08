ad49689ebd2212077eca39b5d1555f98054733b1
Eropso <156892352+Eropso@users.noreply.github.com>
2025-05-08 13:54:22 +0200

    did not do anything. Take back

--- database.php
+++ database.php
@@ -1,6 +1,11 @@
 <?php
 require 'vendor/autoload.php';
 
+if (file_exists(__DIR__ . '/.env')) {
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+    $dotenv->load();
+}
+
 $servername = $_ENV['DB_SERVERNAME'];
 $username = $_ENV['DB_USERNAME'];
 $password = $_ENV['DB_PASSWORD'];
@@ -8,8 +13,7 @@ $dbname = $_ENV['DB_DBNAME'];
 
 
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname;sslmode=require", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch(PDOException $e) {
