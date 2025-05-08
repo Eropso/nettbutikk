8e11e77554475ce38936d4176f6602b1b220281a
Eropso <156892352+Eropso@users.noreply.github.com>
2025-05-08 10:58:28 +0200

    test because cant access user for database
    try to solve Connection failed: SQLSTATE[HY000] [1045] Access denied for user 'eropso@erosho-server'@'40.88.211.83' (using password: YES)

--- database.php
+++ database.php
@@ -2,12 +2,6 @@
 require 'vendor/autoload.php';
 
 
-if (file_exists(__DIR__ . '/.env')) {
-    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-    $dotenv->load();
-}
-
-
 $servername = $_ENV['DB_SERVERNAME'];
 $username = $_ENV['DB_USERNAME'];
 $password = $_ENV['DB_PASSWORD'];
