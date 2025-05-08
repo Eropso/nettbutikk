f5152bee33baa02e7dbea392991119d4c2b76236
Eropso <156892352+Eropso@users.noreply.github.com>
2025-05-08 13:52:59 +0200

    revert to original test
    i dont know if it works just trying at this point

--- database.php
+++ database.php
@@ -1,7 +1,6 @@
 <?php
 require 'vendor/autoload.php';
 
-
 $servername = $_ENV['DB_SERVERNAME'];
 $username = $_ENV['DB_USERNAME'];
 $password = $_ENV['DB_PASSWORD'];
@@ -9,7 +8,7 @@ $dbname = $_ENV['DB_DBNAME'];
 
 
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname;sslmode=require", $username, $password);
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
 
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
