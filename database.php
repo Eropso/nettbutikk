a09cf7ce0caebfbcc4cfaef2904eeca2f8c53678
Eropso <156892352+Eropso@users.noreply.github.com>
2025-05-08 13:02:54 +0200

    tell to use ssl

--- database.php
+++ database.php
@@ -9,7 +9,8 @@ $dbname = $_ENV['DB_DBNAME'];
 
 
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname;sslmode=require", $username, $password);
+
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch(PDOException $e) {
