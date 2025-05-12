eb8859a4a06aeaf88e6568564c7e4ee0d1e6388d
Eropso <156892352+Eropso@users.noreply.github.com>
2025-05-12 09:17:28 +0200

    remove certificate from gitignore

--- database.php
+++ database.php
@@ -1,3 +1,4 @@
+
 <?php
 require 'vendor/autoload.php';
 
@@ -10,13 +11,20 @@ $servername = $_ENV['DB_SERVERNAME'];
 $username = $_ENV['DB_USERNAME'];
 $password = $_ENV['DB_PASSWORD'];
 $dbname = $_ENV['DB_DBNAME'];
+$certPath = __DIR__ . '/ssl/BaltimoreCyberTrustRoot.crt.pem';
 
+$dsn = "mysql:host=$servername;dbname=$dbname;charset=utf8mb4";
+
+$options = [
+    PDO::MYSQL_ATTR_SSL_CA => $certPath,
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+];
 
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname;sslmode=require", $username, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch(PDOException $e) {
+    $conn = new PDO($dsn, $username, $password, $options);
+    echo "Connected successfully with SSL!";
+} catch (PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
 }
 ?>
