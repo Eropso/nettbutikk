0355d7d7c719a8ff9a4621f0514170559a887c06
Eropso <156892352+Eropso@users.noreply.github.com>
2025-05-23 10:17:18 +0200

    Update verification.php

--- authentication/verification.php
+++ authentication/verification.php
@@ -48,7 +48,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
                     
                     if (isset($_SESSION['login-from-cart'])){
-                        header("Location: ../public/cart.php");
+                        header("Location: ../public/checkout.php");
                         exit();
                     }
                     else{
