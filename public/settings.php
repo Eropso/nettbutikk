945700d20789187b641b925ba72929634e46be0d
Eropso <156892352+Eropso@users.noreply.github.com>
2025-09-26 09:24:28 +0200

    Update settings.php

--- public/settings.php
+++ public/settings.php
@@ -114,8 +114,8 @@ if(isset($_POST["update"])){
             </div>
     </form>
     <?php
-        if($_SERVER['REQEUST_METHOD'] === 'POST'){
-            $twofa_checked = isset($_POST['twofa_checked'] ? 1:0);
+        if($_SERVER['REQUEST_METHOD'] === 'POST'){
+            $twofa_checked = isset($_POST['twofa_checked']) ? 1:0;
             $sql = "UPDATE users SET 2fa_enabled = :twofa_checked WHERE id = :id";
             $stmt = $conn->prepare($sql);
             $stmt->bindParam(':twofa_checked', $twofa_checked);
@@ -127,14 +127,14 @@ if(isset($_POST["update"])){
         $stmt = $conn->prepare($sql);
         $stmt->bindParam(':id', $id);
         $stmt->execute();
-        $verify_result=$stmt->fetch(pdo::FETCH_ASSOC);
+        $verify_result=$stmt->fetch(PDO::FETCH_ASSOC);
 
 
     ?>
     <div>
         <form action="" method="POST">
             <label>2FA</label>
-            <input name="twofa_checked" type="checkbox" <?php echo $verify_result['2fa_enabled'] ? 'checked' : 0 ?>>
+            <input name="twofa_checked" type="checkbox" <?php echo $verify_result['2fa_enabled'] ? 'checked' : '' ?>>
             <button type="submit">Save</button>
         </form>
     </div>
